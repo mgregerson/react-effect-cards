94bf66168b0c278dd98ccd4814b613df655cf89e
Claudia Lam <claudiaslam@gmail.com>
2023-04-25 12:23:38 -0700

    update logic for showing cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import DeckOfCards from "./DeckOfCards";
 
+//TODO: docstring
 function App() {
   return (
     <div className="App">
--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -3,48 +3,72 @@ import { useState, useEffect } from "react";
 
 const BASE_URL = "https://deckofcardsapi.com/api/deck";
 
+/**
+ *
+ * Props: none
+ *
+ * State:
+ * - deck : {success, deck_id, shuffled, remaining}
+ * - cards: [{code, images, image, value, suit}, ...{}]
+ * - remaining: number
+ * - isLoading : boolean
+ *
+ * App -> DeckOfCards
+ *
+ */
+
 function DeckOfCards() {
   const [deck, setDeck] = useState(null);
-  const [card, setCard] = useState([]);
-  const [remaining, setRemaining] = useState(52);
+  const [cards, setCards] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   console.log(deck, "THE DECK STATE");
 
   useEffect(function fetchDeckOnMount() {
     async function fetchDeck() {
-      const deckResult = await axios.get(`${BASE_URL}/new/`);
+      const deckResult = await axios.get(`${BASE_URL}/new/shuffle/`);
       setDeck(deckResult.data);
     }
     fetchDeck();
     setIsLoading(false);
   }, []);
 
-  function getCards() {
-    async function getCard() {
-      const card = await axios.get(`${BASE_URL}/${deck.deck_id}/draw/?count=1`);
-      setRemaining((r) => r - 1);
-      console.log(card, "THE CARD WE DREW");
-    }
-    getCard();
+  async function getCard() {
+    const card = await axios.get(`${BASE_URL}/${deck.deck_id}/draw/?count=1`);
+    setCards((cards) => [...cards, card]);
+
+    console.log(card, "THE CARD WE DREW");
   }
 
   if (isLoading === true) {
     return <div className="Loading">Loading Deck....</div>;
   }
 
-  console.log(remaining, "THE NUMBER REMAINING");
   console.log(deck, "THE DECK STATE AFTER USE EFFECT");
+  console.log(cards, "CARDS WE ADDED");
 
   return (
     <div className="DeckOfCards">
-      {remaining > 0 ? (
-        <button className="DeckOfCards-button" onClick={getCards}>
-          Draw a card
-        </button>
-      ) : (
-        <div></div>
-      )}
+      <div className="Button">
+        {cards.length < 52 ? (
+          <button className="DeckOfCards-button" onClick={getCard}>
+            Draw a card
+          </button>
+        ) : (
+          <button onClick={() => alert("No cards remaining!")}>No Cards</button>
+        )}
+      </div>
+      <div className="Cards">
+        {cards.map((card) => {
+          return (
+            <img
+              key={card.code}
+              src={`${card.image}`}
+              alt={`${card.code}`}
+            ></img>
+          );
+        })}
+      </div>
     </div>
   );
 }
